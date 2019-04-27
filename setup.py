26af930033480cb1da2afd64c956b9c4ae91e072
Jeff Derksen <jeff.a.derksen@gmail.com>
2019-04-27 17:42:23 +0000

    trying to remove src

--- setup.py
+++ setup.py
@@ -1,5 +1,27 @@
-from setuptools import setup, find_packagaes
+from setuptools import setup
+import os
+import sys
 
-setup(  name="soundboard",
-        packages=find_packages()
+_here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(_here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+version = {}
+with open(os.path.join(_here, 'soundboard', 'version.py')) as f:
+    exec(f.read(), version)
+
+setup(
+    name='soundboard',
+    version=version['__version__'],
+    description=('app for a Raspi to be a Soundboard'),
+    author='Jeff Derksen',
+    author_email='jeff.a.derksen@gmail.com',
+    url='https://github.com/Jerksen/soundboard',
+    license='GPL-3.0',
+    packages=['soundboard'],
+    install_requires=[  'toml',
+                        'Pillow',
+                        ''],
+    include_package_data=True,
     )
--- src/ext_interfaces/leds.py
+++
@@ -1,8 +0,0 @@
-import logging
-logging.info("importing dicetower.leds")
-
-class LedMatrix(object):
-    pass
-
-class RgbLed(object):
-    pass
